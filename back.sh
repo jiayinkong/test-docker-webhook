2821f0d4d92150001d8b0db981a0ed4d319c2a79
jiayinkong <ry1n.kong@gmail.com>
2023-11-11 17:46:21 +0800

    update: 修改镜像版本

--- back.sh
+++ back.sh
@@ -12,4 +12,4 @@ echo '停止旧容器并删除旧容器'
 docker stop test-docker-back-conatiner
 docker rm test-docker-back-container
 echo '启动新容器'
-docker container run -p 3000:3000 --name test-docker-back-container -d test-docker-back1.0
+docker container run -p 3000:3000 --name test-docker-back-container -d test-docker-back:1.0
