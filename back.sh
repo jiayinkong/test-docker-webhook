ec09b925a318e5e7fbe5d2673426fffae2119347
jiayinkong <ry1n.kong@gmail.com>
2023-11-11 17:36:08 +0800

    feat: 给镜像添加tag

--- back.sh
+++ back.sh
@@ -7,9 +7,9 @@ git clean -f
 echo '拉取最新代码'
 git pull origin main
 echo '开始执行构建'
-docker build -t test-docker-back .
+docker build -t test-docker-back:1.0 .
 echo '停止旧容器并删除旧容器'
 docker stop test-docker-back-conatiner
 docker rm test-docker-back-container
 echo '启动新容器'
-docker container run -p 3000:3000 --name test-docker-back-container -d test-docker-back
+docker container run -p 3000:3000 --name test-docker-back-container -d test-docker-back1.0
